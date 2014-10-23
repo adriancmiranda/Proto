2148e21b5ff819b673f4897a7da5b171ca3e5584
Adrian Miranda <adriancmiranda@gmail.com>
2014-10-23 18:07:36 -0200

    Update Utils.js

--- source/helpers/Utils.js
+++ source/helpers/Utils.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var DONT_ENUMS, IS_DONTENUM_BUGGY;
+// Class - Utilities methods
 
+var DONT_ENUMS, IS_DONTENUM_BUGGY;
 DONT_ENUMS = ['toString', 'toLocaleString', 'valueOf', 'hasOwnProperty', 'isPrototypeOf', 'propertyIsEnumerable', 'constructor'];
 IS_DONTENUM_BUGGY = (function () {
 	for (var property in { toString: 1 }) {
@@ -11,6 +12,31 @@ IS_DONTENUM_BUGGY = (function () {
 	}
 	return true;
 }());
+
+function iterate(iterable) {
+	var length, results;
+	if (!typeOf(iterable)) {
+		return [];
+	}
+	if ('toArray' in Object(iterable)) {
+		return iterable.toArray();
+	}
+	length = iterable.length || 0;
+	results = new Array(length);
+	while (length--) {
+		results[length] = iterable[length];
+	}
+	return results;
+}
+
+function extend(destination, source) {
+	for (var property in source) {
+		destination[property] = source[property];
+	}
+	return destination;
+}
+
+// Class - Static methods
 
 function getDefinitionName(value, strict) {
 	if (value === false) {
@@ -55,25 +81,75 @@ function typeOf(value, strict) {
 	return value ? type : value;
 }
 
-function iterate(iterable) {
-	var length, results;
-	if (!typeOf(iterable)) {
-		return [];
-	}
-	if ('toArray' in Object(iterable)) {
-		return iterable.toArray();
-	}
-	length = iterable.length || 0;
-	results = new Array(length);
-	while (length--) {
-		results[length] = iterable[length];
-	}
-	return results;
+// Class - Other static methods
+
+function isUndefined(value) {
+	return !typeOf(value);
 }
 
-function extend(destination, source) {
-	for (var property in source) {
-		destination[property] = source[property];
+function isDefined(value) {
+	return !isUndefined(value);
+}
+
+function isObject(value) {
+	return typeOf(value) === 'object';
+}
+
+function isString(value) {
+	return typeOf(value) === 'string';
+}
+
+function isNumber(value) {
+	return typeOf(value) === 'number';
+}
+
+function isUint(value) {
+	return typeOf(value, true) === 'uint';
+}
+
+function isInt(value) {
+	return typeOf(value, true) === 'int';
+}
+
+function isDate(value) {
+	return typeOf(value) === 'date';
+}
+
+function isArray(value) {
+	return typeOf(value) === 'array';
+}
+
+function isArrayLike(value) {
+	if (value == null || isWindow(value)) {
+		return false;
 	}
-	return destination;
+	var length = value.length;
+	if (value.nodeType === 1 && length) {
+		return true;
+	}
+	return isString(value) || isArray(value) || length === 0 || isUint(length) && length > 0 && (length - 1) in value;
+}
+
+function isFunction(value) {
+	return typeOf(value) === 'function';
+}
+
+function isRegExp(value) {
+	return typeOf(value) === 'regexp';
+}
+
+function isBoolean(value) {
+	return typeOf(value) === 'boolean';
+}
+
+function isElement(value) {
+	return !!(value && (value.nodeName || (isFunction(value.on) && isFunction(value.find) && isFunction(value.eq) && isFunction(value.css))));
+}
+
+function isFile(value) {
+	return typeOf(value, true) === 'file';
+}
+
+function isWindow(value) {
+	return value && value.document && value.location && value.alert && value.setInterval && value.setTimeout;
 }
