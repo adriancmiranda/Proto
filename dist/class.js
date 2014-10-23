b0533e94c6fa577df28af48bbe85fae8d7250ec1
Adrian Miranda <adrian.miranda@isobar.com.br>
2014-10-23 18:40:48 -0200

    Update Class

--- dist/class.js
+++ dist/class.js
@@ -1,7 +1,7 @@
 /**
  * Class
  * @description Class is a small JavaScript library for simple but powerful class based inheritance.
- * @version v0.0.2
+ * @version v0.0.3
  * @author Adrian C. Miranda
  * @link https://github.com/adriancmiranda/class.js
  * @license MIT Licensed
@@ -237,110 +237,110 @@
         return extensions;
     }());
     // Based on Alex Arnell's inheritance implementation.
-    var Class = function () {
-            function Subclass() {
+    window.Class = function () {
+        function Subclass() {
+        }
+        function create() {
+            var parent, properties, id;
+            id = -1;
+            parent = null;
+            properties = iterate(arguments);
+            if (typeOf(properties[0]) === 'function') {
+                parent = properties.shift();
             }
-            function create() {
-                var parent, properties, id;
-                id = -1;
-                parent = null;
-                properties = iterate(arguments);
-                if (typeOf(properties[0]) === 'function') {
-                    parent = properties.shift();
+            function Caste() {
+                if (typeOf(this.initialize) === 'function') {
+                    this.initialize.apply(this, arguments);
                 }
-                function Caste() {
-                    if (typeOf(this.initialize) === 'function') {
-                        this.initialize.apply(this, arguments);
-                    }
-                }
-                Object.extend(Caste, Class.Methods);
-                Caste.superclass = parent;
-                Caste.subclasses = [];
-                if (parent) {
-                    Subclass.prototype = parent.prototype;
-                    Caste.prototype = new Subclass();
-                    parent.subclasses.push(Caste);
-                }
-                while (++id < properties.length) {
-                    Caste.implement(properties[id]);
-                }
-                if (!typeOf(Caste.prototype.initialize)) {
-                    Caste.prototype.initialize = function () {
-                    };
-                }
-                Caste.prototype.constructor = Caste;
-                return Caste;
             }
-            function mutate(items) {
+            Object.extend(Caste, Class.Methods);
+            Caste.superclass = parent;
+            Caste.subclasses = [];
+            if (parent) {
+                Subclass.prototype = parent.prototype;
+                Caste.prototype = new Subclass();
+                parent.subclasses.push(Caste);
             }
-            function implement(source) {
-                var ancestor, properties, id, property, value, method;
-                ancestor = this.superclass && this.superclass.prototype;
-                properties = Object.keys(source);
-                id = -1;
-                if (IS_DONTENUM_BUGGY) {
-                    if (source.toString != Object.prototype.toString) {
-                        properties.push('toString');
-                    }
-                    if (source.valueOf != Object.prototype.valueOf) {
-                        properties.push('valueOf');
-                    }
-                }
-                while (++id < properties.length) {
-                    property = properties[id];
-                    value = source[property];
-                    if (property === 'implements') {
-                        if (typeOf(value) === 'function') {
-                        } else {
-                        }
-                    }
-                    if (ancestor && typeOf(value) === 'function' && /\$super/g.test(value.argumentNames()[0])) {
-                        method = value;
-                        value = function (fn) {
-                            return function () {
-                                return ancestor[fn].apply(this, arguments);
-                            };
-                        }(property).wrap(method);
-                        value.valueOf = function (method) {
-                            return function () {
-                                return method.valueOf.call(method);
-                            };
-                        }(method);
-                        value.toString = function (method) {
-                            return function () {
-                                return method.toString.call(method);
-                            };
-                        }(method);
-                    }
-                    this.prototype[property] = value;
-                }
-                return this;
+            while (++id < properties.length) {
+                Caste.implement(properties[id]);
             }
-            return {
-                create: create,
-                Methods: {
-                    extend: Object.extend,
-                    implement: implement
+            if (!typeOf(Caste.prototype.initialize)) {
+                Caste.prototype.initialize = function () {
+                };
+            }
+            Caste.prototype.constructor = Caste;
+            return Caste;
+        }
+        function mutate(items) {
+        }
+        function implement(source) {
+            var ancestor, properties, id, property, value, method;
+            ancestor = this.superclass && this.superclass.prototype;
+            properties = Object.keys(source);
+            id = -1;
+            if (IS_DONTENUM_BUGGY) {
+                if (source.toString != Object.prototype.toString) {
+                    properties.push('toString');
                 }
-            };
-        }();
+                if (source.valueOf != Object.prototype.valueOf) {
+                    properties.push('valueOf');
+                }
+            }
+            while (++id < properties.length) {
+                property = properties[id];
+                value = source[property];
+                if (property === 'implements') {
+                    if (typeOf(value) === 'function') {
+                    } else {
+                    }
+                }
+                if (ancestor && typeOf(value) === 'function' && /\$super/g.test(value.argumentNames()[0])) {
+                    method = value;
+                    value = function (fn) {
+                        return function () {
+                            return ancestor[fn].apply(this, arguments);
+                        };
+                    }(property).wrap(method);
+                    value.valueOf = function (method) {
+                        return function () {
+                            return method.valueOf.call(method);
+                        };
+                    }(method);
+                    value.toString = function (method) {
+                        return function () {
+                            return method.toString.call(method);
+                        };
+                    }(method);
+                }
+                this.prototype[property] = value;
+            }
+            return this;
+        }
+        return {
+            create: create,
+            Methods: {
+                extend: Object.extend,
+                implement: implement
+            }
+        };
+    }();
     // Externalize
-    Class.getDefinitionName = getDefinitionName;
-    Class.typeOf = typeOf;
-    Class.isUndefined = isUndefined;
-    Class.isDefined = isDefined;
-    Class.isObject = isObject;
-    Class.isString = isString;
-    Class.isNumber = isNumber;
-    Class.isUint = isUint;
-    Class.isInt = isInt;
-    Class.isDate = isDate;
-    Class.isArray = isArray;
-    Class.isArrayLike = isArrayLike;
-    Class.isFunction = isFunction;
-    Class.isRegExp = isRegExp;
-    Class.isBoolean = isBoolean;
-    Class.isElement = isElement;
-    Class.isFile = isFile;
-    Class.isWindow = isWindow;
+    window.Class.getDefinitionName = getDefinitionName;
+    window.Class.typeOf = typeOf;
+    window.Class.isUndefined = isUndefined;
+    window.Class.isDefined = isDefined;
+    window.Class.isObject = isObject;
+    window.Class.isString = isString;
+    window.Class.isNumber = isNumber;
+    window.Class.isUint = isUint;
+    window.Class.isInt = isInt;
+    window.Class.isDate = isDate;
+    window.Class.isArray = isArray;
+    window.Class.isArrayLike = isArrayLike;
+    window.Class.isFunction = isFunction;
+    window.Class.isRegExp = isRegExp;
+    window.Class.isBoolean = isBoolean;
+    window.Class.isElement = isElement;
+    window.Class.isFile = isFile;
+    window.Class.isWindow = isWindow;
 }(this, this.document));
