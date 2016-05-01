92b60a9e48477ea22d95cb786e840f472d1d630c
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-05-01 01:00:40 -0300

    Update: complexity

--- source/helpers/implement.js
+++ source/helpers/implement.js
@@ -1,12 +1,13 @@
 define([
 	'./extend',
-	'./isFunction'
-], function(extend, isFunction){
+	'./isFunction',
+	'../common/isArray'
+], function(extend, isFunction, isArray){
 	'use strict';
 
 	function implement(list){
-		list = [].concat(list);
 		var proto = {}, collection = {};
+		list = isArray(list)? list : [list];
 		for(var id = 0, item; id < list.length; id++){
 			item = list[id];
 			if(isFunction(item)){
